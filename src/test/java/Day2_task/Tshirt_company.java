b74a52614d3068728c4721e3a5891e0c4bb58a11
sugunArunkumar <arun24196@gmail.com>
2022-09-20 22:54:00 +0530

    Initial version

--- src/test/java/Day2_task/Tshirt_company.java
+++ src/test/java/Day2_task/Tshirt_company.java
@@ -1,37 +1,41 @@
 package Day2_task;
 
 import java.util.Scanner;
+
 public class Tshirt_company {
 
     int client_total_order;
     String size;
     String colour;
     int pre_order;
+    int value;
 
     static {
         System.out.println("Client already received the 50 small white t-shirts");
     }
 
     Tshirt_company(int pre_order) {
-        this.pre_order=pre_order;
+        this.pre_order = pre_order;
         Scanner sc = new Scanner(System.in);
-        int value = sc.nextInt();
-            if ((pre_order + value < 101) && (!(client_total_order >101))) {
-                size = sc.next();
-                colour = sc.next();
+        value = sc.nextInt();
+        if ((pre_order + value < 101) && (!(client_total_order > 101))) {
+            size = sc.next();
+            colour = sc.next();
 
-                System.out.println("Client need a " + value + " " + size + " size " + colour + " colour t-shirt");
-                client_total_order = pre_order + value;
-                System.out.println("client total order " + client_total_order);
-            } else {
-                System.out.println("client reach the maximum order");
-            }
+            System.out.println("Client need a " + value + " " + size + " size " + colour + " colour t-shirt");
+            client_total_order = pre_order + value;
+            System.out.println("client total order " + client_total_order);
+        } else {
 
-       }
+            System.out.println("client reach the maximum order");
+        }
+    }
 
     public static void main(String[] args) {
         Tshirt_company tc = new Tshirt_company(50);
 
+
     }
+
 
 }
