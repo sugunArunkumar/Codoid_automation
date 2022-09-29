1c6e983fe4abe0a7c47a17137f44ea979bf2cd2d
sugunArunkumar <arun24196@gmail.com>
2022-09-29 20:00:09 +0530

    Initial version

--- src/test/java/Daily_task/Leap_year.java
+++ src/test/java/Daily_task/Leap_year.java
@@ -8,11 +8,13 @@ public class Leap_year {
         System.out.println("Enter an Year :: ");
         Scanner sc = new Scanner(System.in);
         year = sc.nextInt();
+//
+//        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+//            System.out.println("Specified year is a leap year");
+//        else
+//            System.out.println("Specified year is not a leap year");
 
-        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-            System.out.println("Specified year is a leap year");
-        else
-            System.out.println("Specified year is not a leap year");
+        int a = year % 4;
 
     }
 }
